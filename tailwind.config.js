331e62f6fd2202b6ec7235b80198ff2af57d6336
Seray1 <107807594+Seray1@users.noreply.github.com>
2023-06-07 10:23:00 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
         GrayishBlue: "hsl(233, 8%, 62%)",
         LightGrayishBlue: "hsl(220, 16%, 96%)",
         VeryLightGray: "hsl(0, 0%, 98%)",
+        primaryColor:
+          "linear-gradient(to right, hsl(136, 65%, 51%),hsl(192, 70%, 51%))",
       },
     },
     plugins: [],
